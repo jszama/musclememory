04a2ac23cd16adc387438b620a9a640c6570fda9
jszama <jmszamotulski@gmail.com>
2024-01-28 20:55:27 +0000

    Base files, Created rough Header layout

---
+++ frontend/app/components/Header.tsx
@@ -0,0 +1,13 @@
+export default function Header() {
+    return (
+        <nav>
+            {/*
+             DROPDOWN MENU WITH ALL PAGES
+             
+             CENTERED LOGO AND TITLE
+
+             ACCOUNT REFS
+            */}
+        </nav>
+    )
+}
