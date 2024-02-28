6b24938ffa177846d349c99b4658ae0454b6e62a
jszama <jmszamotulski@gmail.com>
2024-02-28 19:41:23 +0000

    UI: header created

--- frontend/app/components/Header.tsx
+++ frontend/app/components/Header.tsx
@@ -1,13 +1,24 @@
+import Image from 'next/image';
+import hamburgerIcon from '/hamburger-icon.svg';
+
 export default function Header() {
     return (
-        <nav>
-            {/*
-             DROPDOWN MENU WITH ALL PAGES
-             
-             CENTERED LOGO AND TITLE
-
-             ACCOUNT REFS
-            */}
+        <nav className="header">
+            <div className="hamburger-menu">
+                <Image src={hamburgerIcon} alt="hamburger-menu" width={64} height={64}/>
+                    
+                <ul className="dropdown-menu">
+                    <li className='dropdown-menu-item'>Get Started</li>
+                    <li className='dropdown-menu-item'>Contact Us</li>
+                    <li className='dropdown-menu-item'>About Us</li>
+                </ul>
+            </div>
+            <div className="header-title">
+                <h1>MuscleMemory</h1>
+            </div>
+            <div className="account-logo">
+                <Image src="" alt="account-logo" height={64} width={64}/>
+            </div>
         </nav>
     )
 }
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -13,8 +13,41 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        'primary-900': "#5912A1",
+        'primary-800': "#7016CA",
+        'primary-700': "#6E30E8",
+        'primary-600': "#7047EB",
+        'primary-500': "#7B68EE",
+        'primary-400': "#9C8CF2",
+        'primary-300': "#B7ACF6",
+        'primary-200': "#C7BFF8",
+        'primary-100': "#D4CCFF",
+  
+        'gray-900': "#404040",
+        'gray-800': "#595959",
+        'gray-700': "#737373",
+        'gray-600': "#8C8C8C",
+        'gray-500': "#9E9E9E",
+        'gray-400': "#B8B8B8",
+        'gray-300': "#D1D1D1",
+        'gray-200': "#EBEBEB",
+        'gray-100': "#F5F5F5",
+  
+        'green-500': "#136C16",
+        'green-400': "#367C39",
+        'green-300': "#4CAF50",
+        'green-200': "#71C174",
+        'green-100': "#A3F5A6",
+  
+        'red-500': "#800000",
+        'red-400': "#C20000",
+        'red-300': "#FF0000",
+        'red-200': "#FF6666",
+        'red-100': "#FFB3B3",
+      },  
     },
-  },
+    },
   plugins: [],
 };
 export default config;
