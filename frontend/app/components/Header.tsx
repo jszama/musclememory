aca47ceb3544dccc1b73bbe73f4b65721b875a3d
jszama <jmszamotulski@gmail.com>
2024-03-01 16:47:28 +0000

    UI: header and global styles updated

--- frontend/app/components/Header.tsx
+++ frontend/app/components/Header.tsx
@@ -1,24 +1,36 @@
-import Image from 'next/image';
-import hamburgerIcon from '/hamburger-icon.svg';
+'use client'
+
+import { useState } from 'react';
+import Image from 'next/image';    
+import Link from 'next/link';
 
 export default function Header() {
+    const [showDropdown, setShowDropdown] = useState(false);
+
+    const toggleDropdown = () => {
+        setShowDropdown(!showDropdown);
+    };
+
     return (
         <nav className="header">
-            <div className="hamburger-menu">
-                <Image src={hamburgerIcon} alt="hamburger-menu" width={64} height={64}/>
-                    
-                <ul className="dropdown-menu">
-                    <li className='dropdown-menu-item'>Get Started</li>
-                    <li className='dropdown-menu-item'>Contact Us</li>
-                    <li className='dropdown-menu-item'>About Us</li>
-                </ul>
+            <div className="hamburger-menu" onClick={toggleDropdown}>
+                <div className='hamburger-icon'>
+                    <Image src={"/hamburger.png"} alt="hamburger-menu" width={64} height={64}/>
+                </div>
+                {showDropdown && (
+                    <ul className="dropdown-menu">
+                        <li className='dropdown-menu-item'><Link href='/get-started'>Get Started</Link></li>
+                        <li className='dropdown-menu-item'><Link href='/contact-us'>Contact Us</Link></li>
+                        <li className='dropdown-menu-item'><Link href='/about-us'>About Us</Link></li>
+                    </ul>
+                )}
             </div>
             <div className="header-title">
-                <h1>MuscleMemory</h1>
+                <Link href='/'><h1>MuscleMemory</h1></Link>
             </div>
-            <div className="account-logo">
-                <Image src="" alt="account-logo" height={64} width={64}/>
+            <div className="account-icon">
+                <Link href='account'><Image src="/user.png" alt="account-logo" height={64} width={64}/></Link>
             </div>
         </nav>
-    )
+    );
 }
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,23 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>
-        Home Page
-      </h1>
+    <main className="home-page">
+      <div className="btn-container">
+        <section className='start-btn'>
+         START
+        </section>
+        <section className='app-btn'>
+          History
+        </section>
+        <section className='app-btn'>
+          Workouts
+        </section>
+        <section className='app-btn'>
+          Stats
+        </section>
+        <section className='app-btn'>
+          Friends
+        </section>
+      </div>
     </main>
   );
 }
