eadb3198105f4e3b096f89727a6fb0acbea7c224
jszama <jmszamotulski@gmail.com>
2024-03-10 20:00:23 +0000

    UI: updated exercise menu page

--- frontend/app/exercise-menu/page.tsx
+++ frontend/app/exercise-menu/page.tsx
@@ -1,9 +1,11 @@
+import Link from "next/link"
+
 export default function Workouts() {
     return (
         <main className="home-page">
             <div className="exercise-menu">
-                <h1>Exercise Bank</h1>
-                <h1>Workouts</h1>
+                <Link href='/exercise-menu/exercise-bank'><h1>Exercise Bank</h1></Link>
+                <Link href='/exercise-menu/workouts'><h1>Workouts</h1></Link>
             </div>
         </main>
     );
