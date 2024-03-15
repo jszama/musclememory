307d018f02b43b4ceaf4176709a34ced6ce41773
jszama <jmszamotulski@gmail.com>
2024-03-15 18:56:22 +0000

    UI: workouts page updated

--- frontend/app/exercise-menu/workouts/page.tsx
+++ frontend/app/exercise-menu/workouts/page.tsx
@@ -39,49 +39,46 @@ const exercises: Exercise[] = [
     }]
         
 const workouts: Workout[] = [
-    {
-        id: 1,
-        name: 'Chest Day',
-        exercises: [exercises[0], exercises[1]]
-    },
-    {
-        id: 2,
-        name: 'Back Day',
-        exercises: [exercises[2], exercises[3]]
-    },
-    {
-        id: 3,
-        name: 'Leg Day',
-        exercises: [exercises[4], exercises[0]]
-    }
+    // { 
+    //     id: 1,
+    //     name: 'Chest Day',
+    //     exercises: exercises
+    // }
 ]
 
-export default function ExerciseBank() {
+export default function Workouts() {
+    const noWorkouts = !workouts.length;
     return (
         <main className="home-page">
             <div className="exercise-container">
                 <h1 className='workout-title'>Workouts</h1>
-                <section className="exercise-bank">
+                {noWorkouts ? 
+                    (<div className='no-workouts'>
+                        <h1>Get started on your fitness goals! Create your first workout plan today</h1>
+                        <button>Create Workout</button>
+                    </div>)
+                    :
+                    (<section className="exercise-bank">
                     <section className="workout-list">
-                    {workouts.map((workout) => (
-                        <div className="workout-block" key={workout.id}>
-                            <div className='workout-card' key={workout.name}>
-                                <h3>{workout.name}</h3>
-                                <div className='workout-info'>
-                                    {workout.exercises.map((exercise) => (
-                                        <div key={exercise.id}>
-                                            <h4>{exercise.name}</h4>
-#                                       </div>
-                                    ))}
+                        {workouts.map((workout) => (
+                            <div className="workout-block" key={workout.id}>
+                                <div className='workout-card' key={workout.name}>
+                                    <h3>{workout.name}</h3>
+                                    <div className='workout-info'>
+                                        {workout.exercises.map((exercise) => (
+                                            <div key={exercise.id}>
+                                                <h4>{exercise.name}</h4>
+                                                #                                       </div>
+                                        ))}
+                                    </div>
                                 </div>
                             </div>
+                        ))}
+                        <div className='workout-add'>
+                            <Image src='/addBtn.png' alt='Add workout' height={36} width={36} onClick={() => { }} />
                         </div>
-                    ))}
-                    <div className='workout-add'>
-                            <Image src='/addBtn.png' alt='Add workout' height={36} width={36} onClick={ () => {}} />
-                    </div>
                     </section>
-                </section>
+                </section>)}
             </div>
         </main>
     );
