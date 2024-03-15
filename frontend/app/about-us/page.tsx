780cacb2ca718905d8c5d81a6c506c214da0840b
jszama <jmszamotulski@gmail.com>
2024-03-15 18:55:57 +0000

    UI: about us added

---
+++ frontend/app/about-us/page.tsx
@@ -0,0 +1,39 @@
+export default function About() {
+    return (
+        <main className="about-page">
+            <h1>
+                About MuscleMemory
+            </h1>
+            <article>
+                MuscleMemory is a fitness app that helps you track your workouts and stay on top of your fitness goals.
+            </article>
+            <div className="about-info">
+                <section>
+                    <h2>
+                        Our Mission
+                    </h2>
+                    <article>
+                        Our mission is to help people achieve their fitness goals by providing them with the tools they need to succeed. We believe that everyone should have access to the resources they need to live a healthy lifestyle.
+                    </article>
+                </section>
+
+                <section>
+                    <h2>
+                        Our Features
+                    </h2>
+                    <article>
+                        Our app has a variety of features to help you stay on top of your fitness goals. You can create custom workout plans, track your progress, and connect with other users to stay motivated. We also provide a library of exercises and workouts to help you get started.
+                    </article>
+                </section>
+            </div>
+            <section>  
+                    <h2>
+                        Get Started
+                    </h2>
+                    <article>
+                        Ready to get started? Create an account and start tracking your workouts today!
+                    </article>
+                </section>
+        </main>
+    );
+}
