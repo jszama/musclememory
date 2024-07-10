dcdaa1342c38cf4476320e2cccf9463193a893c6
jszama <jmszamotulski@gmail.com>
2024-07-10 09:52:28 +0100

    fixed display

--- frontend/app/exercise-menu/workouts/workoutList.tsx
+++ frontend/app/exercise-menu/workouts/workoutList.tsx
@@ -57,7 +57,7 @@ export default function WorkoutList({ workoutsProp }: { workoutsProp: Workout[] 
     
     return (
         <>
-        <section className="exercise-bank h-auto">
+        <section className="exercise-bank h-[65%]">
                 <section className="workout-list">
                     {workoutList()}
                 </section>
