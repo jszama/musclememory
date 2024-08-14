d9e6841e101c38408a0f60ca4d356ffcc915e79b
jszama <jmszamotulski@gmail.com>
2024-08-14 21:21:01 +0100

    debug lines

--- backend/config/index.js
+++ backend/config/index.js
@@ -1,5 +1,8 @@
+console.log(__dirname); // prints the directory of the current module
+console.log(require.resolve('../middleWare/authMiddleware')); // prints the resolved path of the module
+
 const db = require('./database');
-const server = require('./server');
+const server = require('./server'); 
 require('dotenv').config();
 
 const port = process.env.API_PORT;
