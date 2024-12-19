2643cdbc7387507eb40aeee87cc8014b67b84144
jszama <jmszamotulski@gmail.com>
2024-12-19 12:48:50 +0000

    updated aws-sdk version

--- backend/config/upload.js
+++ backend/config/upload.js
@@ -5,7 +5,8 @@ require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_REGION
+  region: process.env.AWS_REGION,
+  signatureVersion: 'v4'
 });
 
 const s3 = new AWS.S3();
